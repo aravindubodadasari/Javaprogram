baa48649da00a9d69147ea20790c83ff49408e4a
akash <akash300383@gmail.com>
2022-06-22 18:17:38 +0530

    done

--- Demo.java
+++ Demo.java
@@ -2,5 +2,6 @@ import java.util.*;
 public class Demo {
 	public static void main(String args[]) {
 		System.out.println("Running system program with jenkin "+new Date());
+		System.out.println("Running java program for Batch7");
 	}
 }
