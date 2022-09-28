01b3fed0889d592150db518a1e9077e8d60748fc
Kaleakash <34272796+Kaleakash@users.noreply.github.com>
2022-09-28 16:58:37 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,8 +2,6 @@ import java.util.*;
 public class Demo {
 	public static void main(String args[]) {
 		System.out.println("Running system program with jenkin "+new Date());
-		System.out.println("Running java program for Batch8");
-		System.out.println("Running this application again");
-		System.out.println("Running this application again and again");
+		
 	}
 }
