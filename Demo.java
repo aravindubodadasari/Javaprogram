af82a84d5f26e8ccee4fae21b85c264afc31fca4
Akash <akash300383@gmail.com>
2023-08-12 22:50:26 +0530

    added new line in demo class

--- Demo.java
+++ Demo.java
@@ -2,5 +2,6 @@ import java.util.*;
 public class Demo {
 	public static void main(String args[]) {
 		System.out.println("Welcome to bootcamp java batch");
+		System.out.println("Welcome once again we are learning CI and CD tool");
 	}
 }
