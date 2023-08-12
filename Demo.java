f4f221c1a53b768ad4b38302ff22ac3a87a33e86
Akash <akash300383@gmail.com>
2023-08-12 22:42:35 +0530

    done some changes in demo.java program

--- Demo.java
+++ Demo.java
@@ -1,12 +1,6 @@
 import java.util.*;
 public class Demo {
 	public static void main(String args[]) {
-		System.out.println("Running system program with jenkin "+new Date());
-		System.out.println("This code done by Akash Kale");
-		int a=10;
-		int b=20;
-		int sum = a+b;
-		System.out.println("Sum of two number is "+sum);
-		System.out.println("Welcome once again");
+		System.out.println("Welcome to bootcamp java batch");
 	}
 }
