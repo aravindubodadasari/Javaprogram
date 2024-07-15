447872998cf5f3f41a1adf5134bbdfb2d2082188
akash <akash300383@gmail.com>
2024-07-15 16:41:51 +0530

    done some changes

--- Demo.java
+++ Demo.java
@@ -1,8 +1,6 @@
+import java.time.*;
 public class Demo {
 	public static void main(String args[]) {
-		System.out.println("Today we are learning Jenkin CI and CD tools");
-		System.out.println("We are creating more than one jenkin jobs");
-		String name = "Steven";
-		System.out.println("Welcome user "+name);
+	System.out.println("This job execute at this time"+LocalDateTime.now());		
 	}
 }
