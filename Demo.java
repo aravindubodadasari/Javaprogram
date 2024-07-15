81d11550cd36a744463d5d13a58df2adad05c531
akash <akash300383@gmail.com>
2024-07-15 17:15:32 +0530

    added new contents in file

--- Demo.java
+++ Demo.java
@@ -2,6 +2,6 @@ import java.time.*;
 public class Demo {
 	public static void main(String args[]) {
 	System.out.println("This job execute at this time"+LocalDateTime.now());	
-	System.out.println("Welcome to CI and CD tool training");	
+	System.out.println("Welcome to CI and CD tool training
 	}
 }
