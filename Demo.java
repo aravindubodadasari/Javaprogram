fecf6f059729fb58f4d101949c976f291acd7053
akash <akash300383@gmail.com>
2024-07-18 09:15:26 +0530

    error present

--- Demo.java
+++ Demo.java
@@ -3,5 +3,5 @@ public class Demo {
 	public static void main(String args[]) {
 	System.out.println("This job execute at this time"+LocalDateTime.now());	
 	System.out.println("Welcome to CI and CD tool training");
-	}
+	
 }
