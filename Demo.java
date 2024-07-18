0191559ef366d2ca2ea30f50d6e2c8b7ea877f1b
akash <akash300383@gmail.com>
2024-07-18 09:17:52 +0530

    remove error

--- Demo.java
+++ Demo.java
@@ -3,5 +3,7 @@ public class Demo {
 	public static void main(String args[]) {
 	System.out.println("This job execute at this time"+LocalDateTime.now());	
 	System.out.println("Welcome to CI and CD tool training");
+	System.out.println("Welcome once again");
 	
+	}
 }
