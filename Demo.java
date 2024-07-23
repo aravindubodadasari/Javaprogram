fb0e18bcc9b3a72bdc6ea9ad1c7e1d9c1f6cce42
akash <akash300383@gmail.com>
2024-07-23 07:40:54 +0530

    modify the file

--- Demo.java
+++ Demo.java
@@ -1,9 +1,5 @@
-import java.time.*;
 public class Demo {
-	public static void main(String args[]) {
-	System.out.println("This job execute at this time"+LocalDateTime.now());	
-	System.out.println("Welcome to CI and CD tool training");
-	System.out.println("Welcome once again");
-	
-	}
+    public static void main(String[] args) {
+        System.out.println("Welcome to Jenkin Pipe line job");
+    }
 }
