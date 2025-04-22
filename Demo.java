92392486c8146a23c9cd91c2374df8b6efedebfc
Akash <akash300383@gmail.com>
2025-04-22 07:42:01 +0530

    code contains some error

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Welcome to Jenkin Pipe line job");
-        System.out.println("Welcome to Jenkin job once again");
-    }
+        System.out.println("Welcome to Jenkin job once again")
+    
 }
