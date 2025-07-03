e2f3d9d0dec838079794d6121bcd93c8f70ff380
Akash <akash300383@gmail.com>
2025-07-03 08:19:51 +0530

    done some changes in file

--- Demo.java
+++ Demo.java
@@ -1,8 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
     System.out.println("Today we are learning Jenkin with Trigger options");
-    System.out.println("Welcome to Jenkins CI and CD tool ");
-    System.out.println("This is a simple Java program to demonstrate Jenkins integration.");
     }
     
 }
