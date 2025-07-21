6a0c8b7f075d3d2550eb8d4729559a84d7f35e35
Akash <akash300383@gmail.com>
2025-07-21 17:29:35 +0530

    done

--- Demo.java
+++ Demo.java
@@ -3,6 +3,6 @@ public class Demo {
     System.out.println("Today we are learning Jenkin with Trigger options");
     System.out.println("We re-name branch as main");
      System.out.println("Java project with trigger option enable ");
-    }
+    
     
 }
