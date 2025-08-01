0cac6864e9aa8cd9b268b58dae7f1383a45caa78
aravindu <aravindb402@gmail.com>
2025-08-01 12:53:49 +0530

    Task done

--- Demo.java
+++ Demo.java
@@ -2,7 +2,5 @@ public class Demo {
     public static void main(String[] args) {
     System.out.println("Today we are learning Jenkin");
     System.out.println("We are commiting some changes");
-   
-    
-    
-}
+   }
+ }
